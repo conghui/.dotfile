69d9bfcc90a520bc03a3d8b0a8c24b4080ee3637
Conghui He <heconghui@gmail.com>
2014-12-21 11:12:50 +0800

    add setup for osx

--- aliases.sh
+++ aliases.sh
@@ -1,34 +1,22 @@
 # system depending aliases
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
-  alias ls="ls --color=auto "
+  alias go='gnome-open'
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-  export CLICOLOR=1
-  export LSCOLOR="ExGxBxDxCxEgEdxbxgxcxd"
-  alias ls="ls -G "
-  alias vim="mvim -v"
+  alias go='open'
 fi
 
-alias e="vim"
+alias ls="ls --color=auto "
+alias l='ls'
 alias sl='ls'
 alias ll="ls -lrt"
-alias l='ll'
-
 alias cd..='cd ..'
-
 alias sshx='ssh -X'
-
 alias pbcopy='xclip -selection clipboard'
 
 # in this manner, a nice command:  h <searchterm>
 alias h='history | grep $1'
 
-# open the file using gnome default program
-alias go='gnome-open'
-
-# alias only for zsh
-alias zshconfig="vim ~/.zshrc"
 alias valgrind="valgrind --leak-check=full"
-
 alias nsight='nsight &'
 
 # vim: ft=zsh
--- vars.sh
+++ vars.sh
@@ -55,6 +55,16 @@ function source_if_exist()
   fi
 }
 
+function setup_osx()
+{
+  if command -v brew &> /dev/null; then
+    prepend_PATH "$(brew --prefix coreutils)/libexec/gnubin"
+
+    # GNU manpages for programs that are GNU ones, and fallback to OSX manpages otherwise
+    alias man='_() { echo $1; man -M $(brew --prefix)/opt/coreutils/libexec/gnuman $1 1>/dev/null 2>&1;  if [ "$?" -eq 0 ]; then man -M $(brew --prefix)/opt/coreutils/libexec/gnuman $1; else man $1; fi }; _'
+  fi
+}
+
 ########################## End of function definition #####################
 
 EDITOR=vim
@@ -121,6 +131,8 @@ prepend_PATH ${INSTALL_ROOT}/../bin
 
 # sbin
 prepend_PATH /usr/sbin/
+
+setup_osx
 
 # autojump
 [[ -s ${HOME}/.autojump/etc/profile.d/autojump.sh ]] && \
