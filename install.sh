35b0b7b069f5403dc59224a44f6791584ce4a297
Conghui He <heconghui@gmail.com>
2015-02-01 12:52:40 +0800

    add solarized gnome terminal

--- install.sh
+++ install.sh
@@ -12,7 +12,6 @@ function link_file()
   ln -sf $TRUE $SOFT
 }
 
-
 link_file `pwd`/gdb/gdbinit      ${HOME}/.gdbinit
 link_file `pwd`/git/gitconfig    ${HOME}/.gitconfig
 link_file `pwd`/vim              ${HOME}/.vim
@@ -20,6 +19,10 @@ link_file `pwd`/vim/vimrc        ${HOME}/.vimrc
 link_file `pwd`/tmux/tmux.conf   ${HOME}/.tmux.conf
 link_file `pwd`/zsh/aliases.sh   ${HOME}/.aliases.sh
 link_file `pwd`/zsh/vars.sh      ${HOME}/.vars.sh
+
+# install solarized
+echo "install solarized"
+(cd solarized; zsh install.zsh)
 
 # install zsh
 echo "install zsh"
---
+++ solarized/install.zsh
@@ -0,0 +1,4 @@
+#!/bin/zsh
+
+# clone required repo
+git clone git@github.com:Anthony25/gnome-terminal-colors-solarized.git gnome
